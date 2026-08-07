30ddd8c707eed524430676d1f4c912b838c49fd3
rachitop7790-eng <rachitop7790@gmail.com>
2026-08-07 10:26:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "RexoBoiz Converter Backend is Running!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
